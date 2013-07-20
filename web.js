18384daa025277d8142ab64b2776fdb99e2d7a6a
Suresh Sambasivam <suresh.sambasivam@gmail.com>
2013-07-20 20:49:36 +0000

    Read contents from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    Buffer buffer = fs.readFileSync("index.html");
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
