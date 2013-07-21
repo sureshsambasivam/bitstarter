d5a4e5d7a9c3b54be358caa1fde30e440ded8fbb
Suresh Sambasivam <suresh.sambasivam@gmail.com>
2013-07-21 07:02:31 +0000

    Corrected web.js error

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    Buffer buffer = fs.readFileSync("index.html");
+    var buffer = fs.readFileSync("index.html");
     response.send(buffer.toString());
 });
 
